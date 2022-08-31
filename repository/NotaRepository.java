06a0e5c7de5ae0d929e2317ff05d81f8e2a1ca12
bastosgabriel312 <bastosgabriel312@gmail.com>
2022-08-31 15:04:10 -0300

    :tada: Commit inicial

--- repository/NotaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.linux.notes.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.linux.notes.model.Nota;
-
-public interface NotaRepository extends JpaRepository<Nota, Long>{
-	
-	}
